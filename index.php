98eecffcd167f896f242c0589b011e8fa185a6e5
Matt T Myers <avatarofvelius@users.noreply.github.com>
2017-04-28 19:18:31 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,55 @@
+<?php
+
+	/*
+	<?php include "design.php"; ?>
+	<?php include "connection.php"; ?>
+	*/
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+	<link rel="stylesheet" media="screen" href="index.css" />
+	<!-- <link rel="stylesheet" media="screen" href="css/s_.css" /> -->
+
+	<!-- Script Libraries -->
+	<script type="text/javascript" src="_library/jquery.js"></script>
+
+	<!-- Meta Information -->
+	<title>PHP Template 4192017</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=3.0">
+	<meta charset="utf-8">
+	<meta name="description" content="PHP Template">
+	<meta name="author" content="Matt T. Myers">
+
+</head>
+
+<body>
+
+	<!-- <?php display_header(); ?> -->
+
+	<!-- Content -->
+
+	<!-- Vue Application
+	<div id="app">
+		Vue Application
+	</div>
+	-->
+
+	<!-- Script Libraries -->
+	<script src="_library/vue.js"></script>
+	<!-- Scripts -->
+	<script type="text/javascript">
+
+	</script>
+
+	<!-- <?php display_footer(); ?> -->
+
+</body>
+
+
+
+</html>
